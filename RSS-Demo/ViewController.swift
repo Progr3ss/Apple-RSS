39186cae5c15c013d8a9a18407f17ace22d75df3
Martin <gageincdev@gmail.com>
2019-04-03 01:13:52 -0700

    Initial Commit

---
+++ RSS-Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RSS-Demo
+//
+//  Created by Martin Chibwe on 4/3/19.
+//  Copyright © 2019 Martin Chibwe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
